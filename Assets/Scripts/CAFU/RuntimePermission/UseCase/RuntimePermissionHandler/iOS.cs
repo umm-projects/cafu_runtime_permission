329bb0132bfc2700859502bc14eeee17147c4dbf
Tetsuya Mori <monry84@gmail.com>
2018-03-30 11:46:19 +0900

    :sparkles: Immediate return if already permitted

--- Assets/Scripts/CAFU/RuntimePermission/UseCase/RuntimePermissionHandler/iOS.cs
+++ Assets/Scripts/CAFU/RuntimePermission/UseCase/RuntimePermissionHandler/iOS.cs
@@ -11,6 +11,9 @@ namespace CAFU.RuntimePermission.Domain.UseCase.RuntimePermissionHandler {
         }
 
         public IObservable<bool> RequestPermission(UserAuthorization userAuthorization) {
+            if (this.HasPermission(userAuthorization)) {
+                return Observable.Return(true);
+            }
             Application.RequestUserAuthorization(userAuthorization);
             return Observable.EveryApplicationFocus().Where(x => true).Take(1).Select(_ => this.HasPermission(userAuthorization));
         }
