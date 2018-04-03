da7df59bdc515a6c277b6f3b3e9d7a0bf0d5709f
Tetsuya Mori <monry84@gmail.com>
2018-04-03 14:38:19 +0900

    :beetle: Fix stream handling
    * Amb だと、ストリームの起点時点で制御が走ってしまうもよう
    * SelectMany による連結では無く Buffer による処理に変更

--- Assets/Scripts/CAFU/RuntimePermission/UseCase/RuntimePermissionHandler.cs
+++ Assets/Scripts/CAFU/RuntimePermission/UseCase/RuntimePermissionHandler.cs
@@ -17,21 +17,24 @@ namespace CAFU.RuntimePermission.Domain.UseCase {
         public static UniRx.IObservable<bool> CreateRuntimePermissionDialogResultObservable(this IRuntimePermissionHandler runtimePermissionHandler, UserAuthorization userAuthorization) {
             return Observable
                 // ReSharper disable once InvokeAsExtensionMethod
-                .Amb(
+                .Merge(
                     // 1秒経ってもアプリケーションのフォーカスが外れなかった場合
-                    // ダイアログが出なかったと見なして、その時点でのパーミッションを返す（値は多分偽になる）
+                    // ダイアログが出なかったと見なして、その時点でのパーミッションを返す
+                    // この場合、流れる値は現在の端末設定に依存する
                     Observable
                         .Timer(TimeSpan.FromSeconds(1.0))
                         .TakeUntil(
-                            Observable.EveryApplicationFocus().Where(x => !x).Take(1)
+                            Observable.EveryApplicationFocus().Where(x => !x)
                         )
                         .Select(_ => runtimePermissionHandler.HasPermission(userAuthorization)),
                     // アプリケーションのフォーカスが外れた後に復帰してきた場合
                     // ダイアログが出たと見なして、その時点でのパーミッションを返す（値はユーザの選択によって変動しうる）
                     Observable
-                        .ReturnUnit()
-                        .SelectMany(_ => Observable.EveryApplicationFocus().Where(x => !x).Take(1))
-                        .SelectMany(_ => Observable.EveryApplicationFocus().Where(x => x).Take(1))
+                        .EveryApplicationFocus()
+                        .Buffer(2)
+                        // フォーカスが外れる→フォーカスが戻ってくるの流れになったら OS ダイアログの何らかのボタンが押されたと見なせる
+                        .Where(result => !result[0] && result[1])
+                        .Take(1)
                         .Select(_ => runtimePermissionHandler.HasPermission(userAuthorization))
                 );
         }
